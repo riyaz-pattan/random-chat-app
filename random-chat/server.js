ebad13a56ba33d55848534c7619aea2719809c3e
riyaz-pattan <riyazpattan987@gmail.com>
2025-02-06 09:22:09 +0530

    Finalized chat app for Vercel deployment

--- random-chat/server.js
+++ random-chat/server.js
@@ -15,30 +15,19 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// Store connected users
-let users = [];
-
 // Handle socket connections
 io.on('connection', (socket) => {
   console.log('A user connected');
 
-  // Assign a random username
-  const username = `User${Math.floor(Math.random() * 1000)}`;
-  users.push({ id: socket.id, username });
+  // Send a welcome message
+  socket.emit('system', "Welcome to Random Chat!");
 
-  // Notify the user
-  socket.emit('system', `You are connected as ${username}`);
-  socket.broadcast.emit('system', `${username} has joined the chat`);
-
-  // Handle messages
+  // Handle chat messages
   socket.on('chat message', (msg) => {
-    io.emit('chat message', { user: username, text: msg });
+    io.emit('chat message', msg);
   });
 
-  // Handle disconnects
   socket.on('disconnect', () => {
-    users = users.filter((user) => user.id !== socket.id);
-    io.emit('system', `${username} has left the chat`);
     console.log('A user disconnected');
   });
 });
@@ -48,3 +37,5 @@ const port = process.env.PORT || 3000;
 server.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
+
+module.exports = app;  // Required for Vercel
